232162ddeedef80919d4cf55aa23f5ae89b1b68c
unknown <you@example.com>
2026-06-08 12:06:28 +0530

    Files added

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({ baseURL: '/api' })
+const API=import.meta.env.VITE_API_URL;
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
